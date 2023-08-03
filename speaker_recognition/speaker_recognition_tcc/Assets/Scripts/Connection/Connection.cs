523d6d47a1487bc8576e18488bcfa2ae0e48dca0
ReiAkio <tanaka1557@gmail.com>
2023-08-03 03:38:57 -0300

    Connection with flaskEndpoint running

--- speaker_recognition/speaker_recognition_tcc/Assets/Scripts/Connection/Connection.cs
+++ speaker_recognition/speaker_recognition_tcc/Assets/Scripts/Connection/Connection.cs
@@ -1,8 +1,50 @@
+// Library Imports
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Connection : MonoBehaviour
 {
     
+    // Making the API Request
+    IEnumerator SendPredictionRequest()
+    {
+        // string ngrokUrl = "http://randomstring.ngrok.io";
+        // string apiUrl = ngrokUrl + "/";
+        string apiUrl = "http://127.0.0.1:5000/prediction";
+
+        UnityWebRequest webRequest = UnityWebRequest.Get(apiUrl);
+        yield return webRequest.SendWebRequest();
+
+
+        if (webRequest.result != UnityWebRequest.Result.Success)
+    {
+        Debug.LogError("Error: " + webRequest.error);
+    }
+    else
+    {
+        string responseText = webRequest.downloadHandler.text;
+        Debug.Log("Response: " + responseText);
+    }
+}
+
+[System.Serializable]
+public class PredictionResponse
+{
+    public string predicao;
+}
+
+public void OnPredictionTest()
+{
+     StartCoroutine(SendPredictionRequest());
+}
+
+
+public void Start()
+{
+    OnPredictionTest();
+}
+
 }
