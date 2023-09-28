ebab52a4b4d119e8ab0cb584e85a9a35cc0c5360
ReiAkio <tanaka1557@gmail.com>
2023-09-28 11:20:51 -0300

    Room Actions V1

---
+++ speaker_recognition/speaker_recognition_tcc/Assets/Scripts/Actions/PlayerActions.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+public float speed = 5.0f;
+    private bool moveToTarget = false;
+    public Transform targetTransform;
+    private Animator playerAnimator;
+    private Animator targetAnimator;
+
+    // Novos sprites para cada objeto
+    public Sprite tvNewSprite; 
+    public Sprite doorNewSprite; 
+    public Sprite windowNewSprite;
+
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HandleInput();
+        
+        if (moveToTarget && targetTransform != null)
+        {
+            Vector2 direction = (targetTransform.position - transform.position).normalized;
+            transform.position += (Vector3)direction * speed * Time.deltaTime;
+
+            if (Vector2.Distance(transform.position, targetTransform.position) < 0.1f)
+            {
+                moveToTarget = false;
+                playerAnimator.SetBool("IsWalking", false);
+                ExecuteTargetInteraction();
+            }
+        }
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            targetTransform = GameObject.FindGameObjectWithTag("Tv")?.transform;
+            InitiateMovement();
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            targetTransform = GameObject.FindGameObjectWithTag("Door")?.transform;
+            InitiateMovement();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            targetTransform = GameObject.FindGameObjectWithTag("Window")?.transform;
+            InitiateMovement();
+        }
+    }
+
+    void InitiateMovement()
+    {
+        if (targetTransform != null)
+        {
+            moveToTarget = true;
+            playerAnimator.SetBool("IsWalking", true);
+        }
+    }
+
+    void ExecuteTargetInteraction()
+    {
+        if (targetTransform == null) return;
+
+        targetAnimator = targetTransform.GetComponent<Animator>();
+        if (targetAnimator != null)
+        {
+            targetAnimator.SetTrigger("Activate");
+        }
+        
+        SpriteRenderer sr = targetTransform.GetComponent<SpriteRenderer>();
+        if (sr != null)
+        {
+            switch (targetTransform.tag)
+            {
+                case "Tv":
+                    sr.sprite = tvNewSprite;
+                    break;
+                case "Door":
+                    sr.sprite = doorNewSprite;
+                    break;
+                case "Window":
+                    sr.sprite = windowNewSprite;
+                    break;
+            }
+        }
+    }
+}
